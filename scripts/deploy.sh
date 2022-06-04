d091f089d7cc325cbd685f17673d9cde11a8c969
jaesoek <ljs981120@gmail.com>
2022-06-04 14:33:58 +0900

    deploy.sh 작성

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-echo "test"
+
+APP="duo"
+PROCCESS=$(pgrep -f "npm start")
+
+# 프로세스가 실행 중이면 종료
+if [ -n ${PROCCESS} ]; then
+        echo "실행 중인 애플리케이션을 종료합니다."
+        pm2 delete ${APP}
+fi
+
+echo "최신 애플리케이션을 실행합니다."
+pm2 start npm --watch --name "${APP}" -- start
