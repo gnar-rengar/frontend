860d803e005d3f848e12cbb9404b801920e00d47
JhsK <qwe6293@nate.com>
2022-06-16 23:07:44 +0900

    [프로젝트 세팅] css 초기화 (#6)

---
+++ src/theme/globalStyle.tsx
@@ -0,0 +1,31 @@
+import { css, Global } from '@emotion/react';
+import reset from 'emotion-reset';
+import React from 'react';
+
+function GlobalStyle() {
+  return (
+    <Global
+      styles={css`
+        ${reset} html, body, #__next {
+          margin: 0;
+          padding: 0;
+          height: 100%;
+        }
+        * {
+          box-sizing: border-box;
+        }
+        body {
+          overflow-x: hidden;
+          width: 100%;
+          font-family: 'Pretendard Variable', notokr, 'Nanum Gothic', 'Malgun Gothic', sans-serif;
+        }
+        a {
+          text-decoration: none;
+          outline: none;
+        }
+      `}
+    />
+  );
+}
+
+export default GlobalStyle;
