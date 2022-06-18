1f48a04a9d82ababb1d52cd0b6303a6f1c67fa62
JhsK <qwe6293@nate.com>
2022-06-18 15:43:53 +0900

    ssr 버그로 인한 코드 제거

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -16,19 +16,4 @@ export const handlers = [
     ];
     return res(ctx.json(users));
   }),
-  rest.get('https://backend.api/users', (req, res, ctx) => {
-    const users = [
-      {
-        id: '1',
-        name: '재석',
-        nickname: '썩은김치',
-      },
-      {
-        id: '2',
-        name: '성규',
-        nickname: 'JjsK',
-      },
-    ];
-    return res(ctx.json(users));
-  }),
 ];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
 
-interface User {
-  id: string;
-  name: string;
-  nickname: string;
-}
-
-function Home({ users }: { users: User[] }) {
+function Home() {
   useEffect(() => {
     (async () => {
       const { data } = await axios('/users');
@@ -15,24 +9,7 @@ function Home({ users }: { users: User[] }) {
       console.log(data);
     })();
   });
-  return (
-    <>
-      {users.map((user) => (
-        <div key={user.id}>{user.name}</div>
-      ))}
-    </>
-  );
-}
-
-export async function getServerSideProps() {
-  const res = await fetch('https://backend.api/users');
-  const users: User[] = await res.json();
-
-  return {
-    props: {
-      users,
-    },
-  };
+  return <div>test</div>;
 }
 
 export default Home;
