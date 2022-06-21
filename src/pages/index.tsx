bbd1791ab828a93a68f56d03df1c525332363f53
jaesoek <ljs981120@gmail.com>
2022-06-21 15:52:04 +0900

    필요 없는 코드 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,7 @@
-import React, { useRef, useState } from 'react';
-import CheckBox from '../components/common/CheckBox';
-import Chip from '../components/common/Chip';
-import TextField from '../components/common/TextField';
-import Typography from '../components/common/Typography';
+import React from 'react';
 
 function Home() {
-  const [, setInputValue] = useState('');
-
-  const ref = useRef(null);
-
-  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    setInputValue(e.target.value);
-  };
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '32px' }}>
-      <TextField ref={ref} onChange={handleChange} name="" placeholder="닉네임" />
-      <Chip chosen>Chip</Chip>
-      <CheckBox label="click" checked />
-      <Typography variant="h3">hello world</Typography>
-    </div>
-  );
+  return <div>Home</div>;
 }
 
 export default Home;
