806e2517257955c7deb1b35a91bb347af23d0125
jaesoek <ljs981120@gmail.com>
2022-06-22 12:04:10 +0900

    체크박스 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import CheckBox from '../components/common/CheckBox';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <CheckBox label="만나서 반갑습니다" />
+    </div>
+  );
 }
 
 export default Home;
