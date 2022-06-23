79f2572168dc4530528b9ea1776a72429301c2ad
JhsK <qwe6293@nate.com>
2022-06-23 11:33:02 +0900

    충돌 수정

---
+++ src/dictionary/index.ts
@@ -0,0 +1,17 @@
+const playStyle = [
+  '탑신병자',
+  '교전지향',
+  '뇌지컬',
+  '로밍위주',
+  '탑시팅',
+  '성장 위주',
+  '피드백 환영',
+  '즐겜',
+  '멘탈 좋음',
+] as const;
+
+const position = ['탑', '정글', '미드', '원딜', '서폿'] as const;
+
+const voiceChannel = ['디스코드', '롤 인보이스', '토크온', '기타'] as const;
+
+export { playStyle, position, voiceChannel };
