0b9be570de3cc10e2c702f2fdc0efc13e87d526f
JhsK <qwe6293@nate.com>
2022-06-23 23:56:00 +0900

    api 모킹 작성

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -49,4 +49,22 @@ export const handlers = [
     //   );
     // }
   }),
+
+  rest.patch('/auth/onboarding', (req, res, ctx) => {
+    try {
+      return res(
+        ctx.json({
+          success: true,
+          message: '추가정보 등록에 성공하였습니다.',
+        })
+      );
+    } catch (error) {
+      return res(
+        ctx.json({
+          success: false,
+          message: '추가정보 등록에 실패하였습니다.',
+        })
+      );
+    }
+  }),
 ];
--- src/types/onBoarding.type.ts
+++ src/types/onBoarding.type.ts
@@ -1,9 +1,16 @@
 export interface OnBoardingInput {
   nickName: string;
+<<<<<<< HEAD
   nickNameCheck: boolean;
   playStyle: string[];
   position: string[];
   communication: string;
   useVoice: boolean;
   voiceChannel?: string[];
+=======
+  playStyle: string[];
+  position: string[];
+  communication: string[];
+  voiceChannel: string[];
+>>>>>>> 79d3c92 (api 모킹 작성)
 }
