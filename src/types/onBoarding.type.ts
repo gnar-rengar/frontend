daee5634067845a0cd184cfd4c8f32abcaa105e5
JhsK <qwe6293@nate.com>
2022-06-23 23:56:00 +0900

    api 모킹 작성

--- src/types/onBoarding.type.ts
+++ src/types/onBoarding.type.ts
@@ -2,7 +2,12 @@ export interface OnBoardingInput {
   nickName: string;
   playStyle: string[];
   position: string[];
+<<<<<<< HEAD
   communication: string;
   useVoice: boolean;
   voiceChannel?: string[];
+=======
+  communication: string[];
+  voiceChannel: string[];
+>>>>>>> 21c2f9a (api 모킹 작성)
 }
