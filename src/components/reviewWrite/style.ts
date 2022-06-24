1a1328bc24c6b8e1a4d4368c2b578819f7b59252
jaesoek <ljs981120@gmail.com>
2022-06-24 16:12:58 +0900

    콤마를 세미콜론으로 수정

--- src/components/reviewWrite/style.ts
+++ src/components/reviewWrite/style.ts
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled';
 
 export const SubmitButtonWrapper = styled.div`
-  position: fixed,
-  bottom: 20px,
-  display: flex,
-  justify-content: center,
-  width: 100%,
+  position: fixed;
+  bottom: 20px;
+  display: flex;
+  justify-content: center;
+  width: 100%;
 `;
