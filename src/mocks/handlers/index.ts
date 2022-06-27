bfd61f1e92833a8461cc041a898ce53331935547
JhsK <qwe6293@nate.com>
2022-06-27 11:01:25 +0900

    onboarding mocking

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,19 +1,69 @@
 import { rest } from 'msw';
 
 export const handlers = [
-  rest.get('/users', (req, res, ctx) => {
-    const users = [
-      {
-        id: '1',
-        name: '재석',
-        nickname: '썩은김치',
-      },
-      {
-        id: '2',
-        name: '성규',
-        nickname: 'JjsK',
-      },
-    ];
-    return res(ctx.json(users));
+  rest.patch('/auth/onboarding', (req, res, ctx) => {
+    try {
+      return res(
+        ctx.json({
+          success: true,
+          message: '추가정보 등록에 성공하였습니다.',
+        })
+      );
+    } catch (error) {
+      return res(
+        ctx.json({
+          success: false,
+          message: '추가정보 등록에 실패하였습니다.',
+        })
+      );
+    }
+  }),
+
+  rest.get('https://api.duo-duo/user/checkNick', async (req, res, ctx) => {
+    const nickName = req.url.searchParams.get('lolNickName');
+
+    res(
+      ctx.json({
+        success: true,
+        profileUrl: nickName,
+        message: '계정이 확인되었습니다.',
+      })
+    );
+
+    // try {
+    //   res(
+    //     ctx.json({
+    //       success: true,
+    //       profileUrl: nickName,
+    //       message: '계정이 확인되었습니다.',
+    //     })
+    //   );
+    // } catch (error) {
+    //   console.log('fasffs');
+    //   res(
+    //     ctx.json({
+    //       success: false,
+    //       message: '존재하지 않는 계정입니다.',
+    //     })
+    //   );
+    // }
+  }),
+
+  rest.patch('/auth/onboarding', (req, res, ctx) => {
+    try {
+      return res(
+        ctx.json({
+          success: true,
+          message: '추가정보 등록에 성공하였습니다.',
+        })
+      );
+    } catch (error) {
+      return res(
+        ctx.json({
+          success: false,
+          message: '추가정보 등록에 실패하였습니다.',
+        })
+      );
+    }
   }),
 ];
