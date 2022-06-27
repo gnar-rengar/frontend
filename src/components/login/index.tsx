a8e2ec55d65b1dda14080f29252625bd5612aff9
JhsK <qwe6293@nate.com>
2022-06-27 19:57:55 +0900

    네이버 로그인 인가코드 작업

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -7,6 +7,7 @@ import { ButtonContainer, LoginContainer } from './style';
 function Login() {
   useKakaoLogin();
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
+  const naverURL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`;
 
   return (
     <LoginContainer>
@@ -17,8 +18,12 @@ function Login() {
             <a>카카오로 로그인</a>
           </Link>
         </Button>
+        <Button size="lg">
+          <Link href={naverURL}>
+            <a>네이버로 로그인</a>
+          </Link>
+        </Button>
         <Button size="lg">구글 아디로 로그인</Button>
-        <Button size="lg">네이버로 로그인</Button>
       </ButtonContainer>
     </LoginContainer>
   );
