8573370f8a7bb9ed1b2d7806d611bff071e90adc
JhsK <qwe6293@nate.com>
2022-06-27 11:44:33 +0900

    주석 제거 및 중복 코드 제거

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -29,41 +29,5 @@ export const handlers = [
         message: '계정이 확인되었습니다.',
       })
     );
-
-    // try {
-    //   res(
-    //     ctx.json({
-    //       success: true,
-    //       profileUrl: nickName,
-    //       message: '계정이 확인되었습니다.',
-    //     })
-    //   );
-    // } catch (error) {
-    //   console.log('fasffs');
-    //   res(
-    //     ctx.json({
-    //       success: false,
-    //       message: '존재하지 않는 계정입니다.',
-    //     })
-    //   );
-    // }
-  }),
-
-  rest.patch('/auth/onboarding', (req, res, ctx) => {
-    try {
-      return res(
-        ctx.json({
-          success: true,
-          message: '추가정보 등록에 성공하였습니다.',
-        })
-      );
-    } catch (error) {
-      return res(
-        ctx.json({
-          success: false,
-          message: '추가정보 등록에 실패하였습니다.',
-        })
-      );
-    }
   }),
 ];
