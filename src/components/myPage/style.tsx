b4e752de14e92322891981f504ff133fb5746a63
jaesoek <ljs981120@gmail.com>
2022-07-01 09:40:38 +0900

    캐시 삭제, 대소문자 변경

---
+++ src/components/myPage/style.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const ProfileCardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 16px 0;
+  gap: 16px;
+`;
+
+export const StyledFeedback = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
