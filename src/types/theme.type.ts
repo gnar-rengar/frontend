d7fbe7dfcdbf0e45bf0eb74db35e3816b6c26763
jaesoek <ljs981120@gmail.com>
2022-07-02 15:08:28 +0900

    배포 서버 dev로 수정

---
+++ src/types/theme.type.ts
@@ -0,0 +1,24 @@
+export type TypographyVariant =
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'body1'
+  | 'body2'
+  | 'body3'
+  | 'body4'
+  | 'caption';
+
+export type ColorVariant =
+  | 'primary'
+  | 'primaryVariant'
+  | 'secondary'
+  | 'error'
+  | 'background'
+  | 'surface'
+  | 'onPrimary'
+  | 'onError'
+  | 'onBackground'
+  | 'onSurface'
+  | 'onBackgroundSub'
+  | 'disable';
