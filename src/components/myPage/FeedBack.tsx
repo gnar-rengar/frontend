487ad87d57d143dcbc781ae52d93a053ab2c3a92
jaesoek <ljs981120@gmail.com>
2022-07-07 11:09:32 +0900

    experimental 삭제

--- src/components/myPage/FeedBack.tsx
+++ src/components/myPage/FeedBack.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from '@emotion/react';
-import Image from 'next/future/image';
+import Image from 'next/image';
 import React from 'react';
 import { Typography } from '../common';
 
