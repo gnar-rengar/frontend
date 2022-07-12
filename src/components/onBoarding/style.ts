439479c72b5fb08eebaf712061426a8039e3292c
jaesoek <ljs981120@gmail.com>
2022-07-12 10:36:08 +0900

    일부 페이지 레이아웃 수정

--- src/components/onBoarding/style.ts
+++ src/components/onBoarding/style.ts
@@ -6,7 +6,6 @@ export const OnBoardingContainer = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 16px 0;
   gap: 8px;
   padding-bottom: calc(48px * 2);
 `;
