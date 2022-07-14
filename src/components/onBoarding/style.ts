49feba7938a7d80b4c4b489e73c31357cb46728a
jaesoek <ljs981120@gmail.com>
2022-07-14 17:54:30 +0900

    온보딩 페이지 레이아웃 수정

--- src/components/onBoarding/style.ts
+++ src/components/onBoarding/style.ts
@@ -3,8 +3,10 @@ import styled from '@emotion/styled';
 export const OnBoardingContainer = styled.form`
   width: 100%;
   height: 100%;
+  min-height: calc(100vh - ${({ theme }) => theme.header.height});
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   align-items: center;
   gap: 8px;
 `;
