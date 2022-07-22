344157b4dd637c21e9abfe93f37dddccea280056
jaesoek <ljs981120@gmail.com>
2022-07-22 16:17:55 +0900

    채팅방 목록 로드

--- src/components/chats/index.tsx
+++ src/components/chats/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { SocketContext } from '../../contexts/socket';
 import { Divider } from '../common';
 import ChatRoomCard from './ChatRoomCard';
 import { ChatContainer } from './style';
@@ -32,13 +33,35 @@ const chatrooms = [
   },
 ];
 
+type Room = {
+  roomId: string;
+  userId: string;
+  lolNickname: string;
+  profileUrl: string;
+  lastMessageText: string;
+  lastMessagedTime: string;
+  unRead: number;
+};
+
 function Chats() {
+  const [rooms, setRooms] = useState<Room[]>([]);
+
+  const socket = useContext(SocketContext);
+
+  useEffect(() => {
+    socket.emit('getChatRoom', '62d509be151f1fb3b2e0f792');
+
+    socket.on('onGetChatRoom', (roomsData: Room[]) => {
+      setRooms(roomsData);
+    });
+  }, [socket]);
+
   return (
     <ChatContainer>
       <Divider />
-      {chatrooms.map((room) => (
-        <React.Fragment key={room.id}>
-          <Link href={`chat/${room.id}`}>
+      {rooms.map((room) => (
+        <React.Fragment key={room.roomId}>
+          <Link href={`chats/${room.roomId}`}>
             <a>
               <ChatRoomCard room={room} />
             </a>
