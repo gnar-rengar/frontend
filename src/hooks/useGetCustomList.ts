c770359a9cdb0043f72827e93bb59edd48a43acf
JhsK <qwe6293@nate.com>
2022-07-25 11:10:41 +0900

    주석 추가

--- src/hooks/useGetCustomList.ts
+++ src/hooks/useGetCustomList.ts
@@ -2,6 +2,7 @@ import { useQuery } from 'react-query';
 import { axios } from '../axios';
 import { queryKeys } from './queryKeys';
 
+// DTO,타입 설정은 API 명세서 수정 이후 작성 예정.
 const customListGetAPI = async () => {
   const { data } = await axios.get('/duo/customList');
   return data;
