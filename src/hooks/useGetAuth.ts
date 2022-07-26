be7f2c39e278a84b8a6c0b68f8c08661dd8594e0
JhsK <qwe6293@nate.com>
2022-07-26 18:22:13 +0900

    로그인 사용자 정보 api 작성

--- src/hooks/useGetAuth.ts
+++ src/hooks/useGetAuth.ts
@@ -1,24 +1,26 @@
+import { AxiosError } from 'axios';
+import { useRouter } from 'next/router';
 import { useQuery } from 'react-query';
 import { axios } from '../axios';
 import { AuthUserDTO } from '../types/api.type';
 import { queryKeys } from './queryKeys';
 
-type AuthUserKeyType = 'success' | 'userId' | 'lolNickname' | 'profileURL';
-
-const authUserGetAPI = async () => {
+export const authUserGetAPI = async () => {
   const { data } = await axios.get<AuthUserDTO>('/auth');
   return data;
 };
 
 const useGetAuth = () => {
-  useQuery(queryKeys.authUser, authUserGetAPI, {
-    onSuccess: (data) => {
-      Object.keys(data).forEach((key: AuthUserKeyType) => {
-        localStorage.setItem(key, data[key] as string);
-      });
+  const router = useRouter();
+  const { data } = useQuery(queryKeys.authUser, authUserGetAPI, {
+    onError: (error: AxiosError) => {
+      if (error.response.status === 401) {
+        router.push('/');
+      }
     },
-    onError: (error) => console.log(error),
   });
+
+  return data;
 };
 
 export default useGetAuth;
