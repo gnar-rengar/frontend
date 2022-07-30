3133b7a4e1c8c40dfc9918c46cf6bb1775db25ab
jaesoek <ljs981120@gmail.com>
2022-07-30 16:48:17 +0900

    주석 제거

--- src/hooks/useGetMatchHistory.ts
+++ src/hooks/useGetMatchHistory.ts
@@ -1,21 +1,3 @@
-// import { useQuery } from 'react-query';
-// import { axios } from '../axios';
-// import { MatchHistoryDTO } from '../types/api.type';
-
-// import { queryKeys } from './queryKeys';
-
-// const fetchMatchHistory = async (userId: string) => {
-//   const { data } = await axios.get<MatchHistoryDTO>(`/user/recentRecord/${userId}`);
-//   return data;
-// };
-
-// const useGetMatchHistory = (userId: string) => {
-//   const { data } = useQuery(queryKeys.matchHistory(userId), () => fetchMatchHistory(userId));
-//   return data;
-// };
-
-// export default useGetMatchHistory;
-
 import { useInfiniteQuery } from 'react-query';
 import { axios } from '../axios';
 import { queryKeys } from './queryKeys';
