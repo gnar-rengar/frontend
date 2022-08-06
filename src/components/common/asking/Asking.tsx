7e8bd9db5e9ee77f4ca668923045c1618ebd94bf
JhsK <qwe6293@nate.com>
2022-08-07 03:39:34 +0900

    asking white space delete

--- src/components/common/asking/Asking.tsx
+++ src/components/common/asking/Asking.tsx
@@ -17,7 +17,7 @@ function Asking(props: AskingProps) {
   return (
     <AskingContainer>
       <TitleContainer>
-        <Typography whiteSpace={whiteSpace} variant="h3" align="left" paragraph={paragraph}>
+        <Typography variant="h3" align="left" paragraph={paragraph}>
           {title}
         </Typography>
         {caption && (
