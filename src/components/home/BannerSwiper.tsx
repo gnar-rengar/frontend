9ad40aad056035f808dd2d595ef24e3416df9c61
JhsK <qwe6293@nate.com>
2022-08-07 20:44:04 +0900

    banner 추가

--- src/components/home/BannerSwiper.tsx
+++ src/components/home/BannerSwiper.tsx
@@ -5,9 +5,12 @@ import Image from 'next/image';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination, Autoplay } from 'swiper';
+import { useRouter } from 'next/router';
 import { SwiperContainer } from './style';
 
 function BannerSwiper() {
+  const router = useRouter();
+
   return (
     <SwiperContainer>
       <Swiper
@@ -18,13 +21,31 @@ function BannerSwiper() {
         autoplay={{ delay: 5000 }}
       >
         <SwiperSlide>
-          <Image src="/banner/home.png" layout="responsive" width="480px" height="256px" />
+          <Image
+            onClick={() => router.push('/banner/manual')}
+            src="/banner/manual.png"
+            layout="responsive"
+            width="480px"
+            height="256px"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Image src="/banner/home.png" layout="responsive" width="480px" height="256px" />
+          <Image
+            onClick={() => router.push('https://tally.so/r/mJ9WXX')}
+            src="/banner/survey.png"
+            layout="responsive"
+            width="480px"
+            height="256px"
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Image src="/banner/home.png" layout="responsive" width="480px" height="256px" />
+          <Image
+            onClick={() => router.push('/banner/gifticon')}
+            src="/banner/gifticon.png"
+            layout="responsive"
+            width="480px"
+            height="256px"
+          />
         </SwiperSlide>
       </Swiper>
     </SwiperContainer>
