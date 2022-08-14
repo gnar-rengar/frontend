7f0c8806223935e5de21a32966061756482b5948
JhsK <69228184+JhsK@users.noreply.github.com>
2022-08-14 22:17:11 +0900

    [온보딩] 워딩 수정 및 오픈 그래프 추가
    * 온보딩 소통 워딩 수정
    
    * opengraph 작성 및 테스트 필요
    
    * next config add

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,19 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <script src="https://developers.kakao.com/sdk/js/kakao.js" />
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="author" content="duohaeduo" />
+          <meta property="og:title" content="찰떡 궁합 듀오 매칭, 듀오해듀오" />
+          <meta
+            property="og:description"
+            content="소환사님을 기다리고 있어요! 나랑 롤 듀오, 해듀오?"
+          />
+          <meta property="og:url" content="https://duoduo.lol" />
+          <meta
+            property="og:image"
+            content="https://duoplz.s3.ap-northeast-2.amazonaws.com/kakaoShare.png"
+          />
         </Head>
         <body>
           <Main />
