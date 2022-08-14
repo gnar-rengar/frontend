fbf7a49216ef0b7cde4fcb65aa43556b797513f2
JhsK <qwe6293@nate.com>
2022-08-14 21:55:53 +0900

    opengraph 작성 및 테스트 필요

--- src/pages/tendency-test.tsx
+++ src/pages/tendency-test.tsx
@@ -1,9 +1,26 @@
+import Head from 'next/head';
 import React, { ReactElement } from 'react';
 import Layout from '../components/layout/Layout';
 import TendencyTest from '../components/tendencyTest';
 
 function TendencyTestPage() {
-  return <TendencyTest />;
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="나의 롤 플레이 스타일은?" />
+        <meta
+          property="og:description"
+          content="듀오해듀오에서 플레이 스타일 테스트하고, 맞춤 듀오 추천받자!"
+        />
+        <meta property="og:url" content="https://duoduo.lol/tendency-test" />
+        <meta
+          property="og:image"
+          content="https://duoplz.s3.ap-northeast-2.amazonaws.com/kakaoShare.png"
+        />
+      </Head>
+      <TendencyTest />
+    </>
+  );
 }
 
 export default TendencyTestPage;
