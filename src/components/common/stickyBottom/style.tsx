6b41fa0c326d65f1c5f92f090e3a29063e546bb8
jaesoek <ljs981120@gmail.com>
2022-08-17 12:05:04 +0900

    sticky-bottom 너비 수정

--- src/components/common/stickyBottom/style.tsx
+++ src/components/common/stickyBottom/style.tsx
@@ -6,7 +6,8 @@ export const StickyBottomContainer = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
-  padding: 24px 0 34px 0;
+  margin-left: -16px;
+  padding: 24px 16px 34px 16px;
   gap: 12px;
   background: linear-gradient(180deg, rgba(18, 18, 18, 0) 0%, #121212 28.13%);
   box-sizing: content-box;
