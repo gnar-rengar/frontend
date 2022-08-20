bb94a20e01fa4513c342ed9d8a1f1e1c7328e8bd
jaesoek <ljs981120@gmail.com>
2022-08-20 19:16:35 +0900

    error-boundary 추가

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Error from './error';
+
+interface Props {
+  children: React.ReactNode;
+  message?: string;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = { hasError: false };
+  }
+
+  public static getDerivedStateFromError(): State {
+    return { hasError: true };
+  }
+
+  public render() {
+    const { children } = this.props;
+    const { hasError } = this.state;
+
+    if (hasError) {
+      return <Error type={500} />;
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
