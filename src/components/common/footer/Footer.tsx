9f1272d849c7c8044c66615f097991b32dd28cfd
JhsK <qwe6293@nate.com>
2022-09-17 17:21:29 +0900

    footer, title 수정

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 import React from 'react';
 import Typography from '../typography/Typography';
-import { FooterContainer, InfoKoreanCotainer, LinkContainer, UnderLineLink } from './Footer.style';
+import { FooterContainer, LinkContainer, UnderLineLink } from './Footer.style';
 
 function Footer() {
   return (
     <FooterContainer>
-      <InfoKoreanCotainer>
+      {/* <InfoKoreanCotainer>
         <Typography variant="captionBold">선데이유니크</Typography>
         <Typography variant="captionRegular">
           대표 신동영 | 사업자등록번호 677-12-01869
@@ -15,7 +15,7 @@ function Footer() {
           <br />
           duohaeduo@gmail.com
         </Typography>
-      </InfoKoreanCotainer>
+      </InfoKoreanCotainer> */}
       <Typography variant="captionRegular">
         © 2022 DUODUO was created under Riot Games&#39; &quot;Legal Jibber Jabber&quot; policy using
         assets owned by Riot Games. Riot Games does not endorse or sponsor this project.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React from 'react';
 import LoadingSuspense from '../components/common/loadingSuspense';
 import Home from '../components/home';
@@ -7,9 +8,14 @@ import { preFetchAuth } from '../hooks/preFetchFns';
 
 function HomePage({ isAuth }: { isAuth: boolean }) {
   return (
-    <LoadingSuspense>
-      <Home isAuth={isAuth} />
-    </LoadingSuspense>
+    <>
+      <Head>
+        <title>듀오해듀오 - DUODUO.LOL</title>
+      </Head>
+      <LoadingSuspense>
+        <Home isAuth={isAuth} />
+      </LoadingSuspense>
+    </>
   );
 }
 
